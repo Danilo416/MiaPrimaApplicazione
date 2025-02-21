522822f44e4e6c26e3197ebda344de8376f8e570
st10751 <st10751@iismarconipieralisi.it>
2025-02-21 11:22:12 +0100

    push commit

--- app/src/main/java/com/example/miaprimaapplicazione/MainActivity.java
+++ app/src/main/java/com/example/miaprimaapplicazione/MainActivity.java
@@ -15,8 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+
 
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent( MainActivity.this, SecondActivity.class);
